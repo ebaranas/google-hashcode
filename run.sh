1803f34a3327b40627ef318fc9ac554e0ddb9c15
Erika B. Aranas <ebaranas@users.noreply.github.com>
2018-11-11 14:13:47 +0000

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,6 @@
+#python3 cars.py < a_example.in > a.out
+#python3 cars.py < b_should_be_easy.in > b.out
+#python3 cars.py < c_no_hurry.in > c.out
+python3 cars.py < d_metropolis.in > d.out
+#python3 cars.py < e_high_bonus.in > e.out
+
